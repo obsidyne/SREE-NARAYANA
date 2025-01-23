ddac7ac8e8f7d0ce8b5a33b21e0c46ce10707cdb
jiyarosejoshy <jiyarosejoshy2004@gmail.com>
2025-01-23 21:44:59 +0530

    nav

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,14 +5,15 @@ export default function Navbar() {
   return (
     <div className="nav-container">
         <div className="nav-head">
-          <h1>AYURMITHRA</h1><span>Eco Wellness Resort</span>
+          <h1>AYURMITHRA</h1>
+          <span>Eco Wellness Resort</span>
         </div>
         <div className="nav-items">
           <ul className="nav-list">
-            <li className="nav-list-item">ABOUT US</li>
-            <li className="nav-list-item">FACILITIES</li>
-            <li className="nav-list-item">BOOKING</li>
-            <li className="nav-list-item">OCNTACT US</li>
+            <li className="nav-list-item"><b>ABOUT US</b></li>
+            <li className="nav-list-item"><b>FACILITIES</b></li>
+            <li className="nav-list-item"><b>BOOKING</b></li>
+            <li className="nav-list-item"><b>CONTACT US</b></li>
           </ul>
         </div>
     </div>
